4595ce97298caa1197194f91fd2f983126a6ca46
Emily Allegretti <allegretti.7@osu.edu>
2023-03-07 21:38:41 -0500

    implemented courses controller and REST operations
    set up relational database
    
    generated devise views/controller

---
+++ Project2/db/schema.rb
@@ -0,0 +1,79 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_025852) do
+  create_table "applications", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
+  create_table "availabilities", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "application_id", null: false
+    t.index ["application_id"], name: "index_availabilities_on_application_id"
+  end
+
+  create_table "course_preferences", id: false, force: :cascade do |t|
+    t.integer "application_id", null: false
+    t.integer "course_id", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer "number"
+    t.string "name"
+    t.string "campus"
+    t.string "term"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.integer "num_graders_required", default: 1, null: false
+    t.integer "section_number", null: false
+    t.string "start_time", null: false
+    t.string "end_time", null: false
+    t.string "location"
+    t.string "days_of_week", null: false
+    t.string "mode_of_instruction"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "section_id", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "fname", null: false
+    t.string "lname"
+    t.boolean "pending_approval?"
+    t.boolean "admin_flag"
+    t.boolean "instructor_flag"
+    t.boolean "student_flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "applications", "users"
+  add_foreign_key "availabilities", "applications"
+end
