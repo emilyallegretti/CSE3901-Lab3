9b40a2ab92438dc2dd9c65ce5cc2da1ed92de571
Mara Beam <beam.142@osu.edu>
2023-03-09 10:47:02 -0500

    Created landing page controller/view

--- Project2/config/routes.rb
+++ Project2/config/routes.rb
@@ -8,4 +8,17 @@ Rails.application.routes.draw do
  resources :courses
 
  get 'courses/reload', to: 'courses#reload'
+  get 'home/index'
+  # Check if the user is signed in. If signed in, go to home page.
+  # Else, go to sign in.
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  get 'courses/reload', to: 'courses#reload'
 end
