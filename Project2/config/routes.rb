658a987b4b0e0ea01cf1150aafe2a298c9c4142b
Mara Beam <beam.142@osu.edu>
2023-03-19 09:17:02 -0400

    Set default page to login screen

--- Project2/config/routes.rb
+++ Project2/config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
 
-  root to: "home#index"
+  # Check if the user is signed in. If signed in, go to home page.
+  # Else, go to sign in.
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   get 'courses/reload', to: 'courses#reload'
 end
