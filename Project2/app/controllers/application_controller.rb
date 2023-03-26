a44784edaf968debcc1e25d160807d3f13229ebc
Emily Allegretti <allegretti.7@osu.edu>
2023-03-26 16:08:30 -0400

    implemented sign-up functionality

--- Project2/app/controllers/application_controller.rb
+++ Project2/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname, :admin_flag, :instructor_flag, :student_flag, :pending_approval?, :email])
+    end
 end
