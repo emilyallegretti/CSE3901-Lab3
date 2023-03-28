154e4f180e3dda5f968cd7082ce369a07b38219f
Emily Allegretti <allegretti.7@osu.edu>
2023-03-28 13:35:21 -0400

    fixed merge errors

--- Project2/db/schema.rb
+++ Project2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_26_181039) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_023921) do
   create_table "applications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
