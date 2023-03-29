26df2d087d93aa418eb49ffc99cc3ac7c476da9b
Aaron Phan <phan.176@osu.edu>
2023-03-29 13:06:08 -0400

    nameing error

--- Project2/app/controllers/dashboard_controller.rb
+++ Project2/app/controllers/dashboard_controller.rb
@@ -1,13 +1,13 @@
 class DashboardController < ApplicationController
     def load_in_database
         #OsuClient.save_data_from_api
-        Parsing.get("col","1234")
+        Parsing.get("col","1234") #params from view
         redirect_to courses_path
     end
     def reload_database
         Section.destroy_all
         Course.destroy_all      
-        OsuClient.save_data_from_api
+        Parsing.get("col","1234") #params from view
         redirect_to courses_path
     end    
     
