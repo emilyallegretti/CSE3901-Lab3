96baffe7fd814b6aa563d3176ac2a00773342724
Emily Allegretti <allegretti.7@osu.edu>
2023-03-28 21:18:26 -0400

    skip authentication on reload to avoid error

--- Project2/app/controllers/dashboard_controller.rb
+++ Project2/app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def load_in_database
         Services::OsuClient.save_data_from_api
         redirect_to courses_path
