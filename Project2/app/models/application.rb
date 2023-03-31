74b96c3c216718d4cbf929855907f8982604a14b
Emily Allegretti <allegretti.7@osu.edu>
2023-03-31 00:10:15 -0400

    got sections to parse in

--- Project2/app/models/application.rb
+++ Project2/app/models/application.rb
@@ -1,3 +1,4 @@
+# This model represents the Applications table in the database.
 class Application < ApplicationRecord
     has_many :availabilities
 end
--- Project2/db/migrate/20230316001634_create_sections.rb
+++ Project2/db/migrate/20230316001634_create_sections.rb
@@ -2,7 +2,7 @@ class CreateSections < ActiveRecord::Migration[7.0]
   def change
     create_table :sections do |t|
       t.integer :num_graders_required, default: 1, null: false
-      t.integer :section_number, null:false
+      t.string :section_number, null:false
       t.string :start_time
       t.string :end_time
       t.string :location
