e6f8b06add5168a106b1012f0f37cbc21a168a49
Avijit Kumar <01kumar.avijit@gmail.com>
2023-04-19 13:59:46 -0400

    edited the controller and view to fit boolean recommend

--- Project3/app/controllers/recommendation_controller.rb
+++ Project3/app/controllers/recommendation_controller.rb
@@ -1,19 +1,21 @@
 class RecommendationController < ApplicationController
   def create
-    @recommendation = Recommendation.new(recommendation_params)
+    # Find the student/user that is being recommended
+    student = User.find_by(email: sections_user_params[:email], role: 'student')
     
-    # Find or create the student by email
-    student = User.find_or_create_by(email: recommendation_params[:email]) do |new_student|
-      # Set default values for the new student if needed
-      new_student.first_name = 'Unknown'
-      new_student.last_name = 'Unknown'
-    end
-    
-    @recommendation.student_id = student.id
-  
-    if @recommendation.save
-      redirect_to @recommendation, notice: 'Recommendation was successfully submitted.'
+    # If the student exists, then create a new section_user that changes the is_recommended attribute to positive otherwise send a failure message to the user
+    if student
+      @sections_user = SectionsUser.new(sections_user_params.merge(user_id: student.id, is_recommended: "true"))
+      
+      # Save the new section user and send a success message to the submitting user
+      if @sections_user.save
+        redirect_to @sections_user, notice: 'Recommendation was successfully submitted.'
+      else
+        render :new
+      end
     else
+      flash.now[:alert] = 'Student not found. Please make sure the student email is correct.'
+      @sections_user = SectionsUser.new(sections_user_params)
       render :new
     end
   end
